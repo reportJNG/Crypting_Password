cd6202e3bc53b31e0cb45ce108b79f04057fa8cd
reportJNG <hamzaremali10@gmail.com>
2026-02-15 19:05:32 -0800

    fixed main and creating create.tsx for new passwords

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,11 +1,24 @@
+"use client";
+import { MousePointerClickIcon } from "lucide-react";
 import Topbar from "./Topbar";
-
+import { useState } from "react";
 export default function Main() {
+  const [creating, setCreating] = useState<boolean>(false);
   return (
     <div>
       <header>
         <Topbar />
       </header>
+      {creating && (
+        <main>
+          <div></div>
+        </main>
+      )}
+      <footer>
+        <div onClick={() => setCreating((prev) => !prev)}>
+          <MousePointerClickIcon />
+        </div>
+      </footer>
     </div>
   );
 }
